a5cda2f067cb262be80510a095c845faf3330007
Yejun <dksdidtkachd@naver.com>
2026-09-23 14:35:18 +0900

    캔버스 초기화 위치 정리

--- LEC05/character_draws.py
+++ LEC05/character_draws.py
@@ -1,6 +1,11 @@
 #pico2d 라이브러리 활성화
 from pico2d import *
 
+#캔버스 열기
+open_canvas(800, 600)
+character = load_image('character.png')
+
+
 def draw_circle():
     print("draw_circle()")
     pass
@@ -12,11 +17,7 @@ def draw_triangle():
     pass
 
 while True:
-    #캔버스 열기
-    open_canvas(800, 600)
     update_canvas()
-
-    character = load_image('character.png')
     character.draw(400, 300)
 
     draw_circle()
