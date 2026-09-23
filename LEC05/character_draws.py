4b297ad3d8125befa9a7d02c5640bb8ba4ba2056
Yejun <dksdidtkachd@naver.com>
2026-09-23 14:28:57 +0900

    도형 그리기 함수 추가

--- LEC05/character_draws.py
+++ LEC05/character_draws.py
@@ -1,19 +1,26 @@
 #pico2d 라이브러리 활성화
 from pico2d import *
 
-#캔버스 열기
-open_canvas(800, 600)
-update_canvas()
-
-character = load_image('character.png')
-update_canvas()
-
-
-delay(10)
-
-while True:
-    character.draw(400, 300)
+def draw_circle():
+    print("draw_circle()")
+    pass
+def draw_square():
+    print("draw_square()")
+    pass
+def draw_triangle():
+    print("draw_triangle()")
     pass
 
-close_canvas()
+while True:
+    #캔버스 열기
+    open_canvas(800, 600)
+    update_canvas()
+
+    character = load_image('character.png')
+    character.draw(400, 300)
+
+    draw_circle()
+    draw_square()
+    draw_triangle()
+    pass
 
